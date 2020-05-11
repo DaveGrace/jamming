b250cdd405392938ca0ded70cf14e67d9d59733a
dave <boadicea45@gmail.com>
2020-05-11 15:36:23 +1000

    11 May 2020

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'reactDOM';
+import 'Playlist.css';
+import TrackList from './Tracklist';
+
+class Playlist extends React.Component {
+    render() {
+        <div className="Playlist">
+            <input defaultValue= {'New Playlist'} />
+            <!-- Add a TrackList component -->
+  <button classNames="Playlist-save">SAVE TO SPOTIFY</button>
+        </div>
+
+
+    }
+}
+export default Playlist;
+
