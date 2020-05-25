912372cda37928acdb5394583fe324f1b9cb614f
dave <boadicea45@gmail.com>
2020-05-25 13:27:44 +1000

    May 25 2020

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'reactDOM';
-import 'Playlist.css';
-import TrackList from './Tracklist';
+import './Playlist.css';
+import TrackList from '../TrackList/TrackList';
+import App from '../App/App';
 
 class Playlist extends React.Component {
     render() {
-        <div className="Playlist">
-            <input defaultValue= {'New Playlist'} />
-            <!-- Add a TrackList component -->
-  <button classNames="Playlist-save">SAVE TO SPOTIFY</button>
-        </div>
+        return (
+            <div className="Playlist">
+            <input defaultValue= {"New Playlist"} />
+                < TrackList tracks={[{name: 'George', artist: 'Glasgow Kiss', album: 'Saturday Nights Alright', id: 1 },
+                { name: 'name2', artist: 'artist2', alb;';;;pum: 'album2', id: 3 },
+                    { name: 'name3', artist: 'artist3', album: 'album3', id: 2 }]}  />              
+                
 
+                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        </div>
+       )
 
     }
 }
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'reactDOM';
 import './SearchBar.css';
 
-
-
-
-
 class SearchBar extends React.Component {
-    render() {
+    render()
+    {
+    return(
         <div className="SearchBar">
             <input placeholder="Enter A Song, Album, or Artist" />
             <button className="SearchButton">SEARCH</button>
-        </div>
+            </div>
+
+
+        )
     }
 }
 export default SearchBar;
